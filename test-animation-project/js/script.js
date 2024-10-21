ffa894beaacb8cfbc7224901aa209ca3d2ff7e42
lookwhosbeck <nick@lookwhosbeck.design>
2024-10-21 08:12:42 -0400

    Cleaned up and consolidated intro animation code, updated the html of the grid page to animate on transform

--- test-animation-project/js/script.js
+++ test-animation-project/js/script.js
@@ -28,57 +28,27 @@ document.addEventListener("DOMContentLoaded", (event) => {
     // HERO BACKGROUND TILE ANIMATION
     // Hero background tiles fade in and out animation
     gsap.timeline().from(".item-121", {
-        opacity: 0,
-        duration: 0.5,
-        delay: 1,
-        yoyo: true,
-        repeat: -1,
-        repeatDelay: 4.25,
+        opacity: 0, duration: 0.5, delay: 1, yoyo: true, repeat: -1, repeatDelay: 3
     });
 
     gsap.timeline().from(".item-23", {
-        opacity: 0,
-        duration: 0.5,
-        delay: 1.25,
-        yoyo: true,
-        repeat: -1,
-        repeatDelay: 4,
+        opacity: 0, duration: 0.5, delay: 1.25, yoyo: true, repeat: -1, repeatDelay: 3
     });
 
     gsap.timeline().from(".item-85", {
-        opacity: 0,
-        duration: 0.5,
-        delay: 1.5,
-        yoyo: true,
-        repeat: -1,
-        repeatDelay: 3.75,
+        opacity: 0, duration: 0.5, delay: 1.5, yoyo: true, repeat: -1, repeatDelay: 3
     });
 
     gsap.timeline().from(".item-45", {
-        opacity: 0,
-        duration: 0.5,
-        delay: 1.75,
-        yoyo: true,
-        repeat: -1,
-        repeatDelay: 3.5,
+        opacity: 0, duration: 0.5, delay: 1.75, yoyo: true, repeat: -1, repeatDelay: 3
     });
 
     gsap.timeline().from(".item-91", {
-        opacity: 0,
-        duration: 0.5,
-        delay: 2,
-        yoyo: true,
-        repeat: -1,
-        repeatDelay: 3.25,
+        opacity: 0, duration: 0.5, delay: 2, yoyo: true, repeat: -1, repeatDelay: 3
     });
 
     gsap.timeline().from(".item-51", {
-        opacity: 0,
-        duration: 0.5,
-        delay: 2.25,
-        yoyo: true,
-        repeat: -1,
-        repeatDelay: 3,
+        opacity: 0, duration: 0.5, delay: 2.25, yoyo: true, repeat: -1, repeatDelay: 3
     });
 
 });
@@ -107,8 +77,8 @@ tl.to(".grid-wrapper", {
 // Background grid blur filter on scroll
 
 console.clear();
-gsap.set(".grid-wrapper", { 
-    filter: "blur(0px)", 
+gsap.set(".grid-wrapper", {
+    filter: "blur(0px)",
 });
 
 gsap.to(".grid-wrapper", {
@@ -123,8 +93,7 @@ gsap.to(".grid-wrapper", {
 });
 
 
-// Scroll Trigger Parallax with GSAP
-// apply parallax effect to any element with a data-speed attribute
+// APPLY SCROLL TRIGGER PARALLAX WITH GSAP BY ADDING [data-speed] ATTRIBUTE IN HTML
 
 gsap.to("[data-speed]", {
     y: (i, el) => (1 - parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window),
@@ -137,36 +106,24 @@ gsap.to("[data-speed]", {
     }
 });
 
-//EMBLEM ANIMATE IN ON-LOAD--->
-gsap.set(".emblem-trigger", {
-    scale: 0,
-    opacity: 0,
-});
 
-gsap.to(".emblem-trigger", {
-    scale: 1,
-    opacity: 1,
-    ease: 'back.inOut',
-    delay: 3,
-});
+// INTRO TEXT ANIMATION TIMELINE ON LOAD
 
-
-//WITH Timelines (cleaner, more versatile)
-// let introTl = gsap.timeline({delay: 0.75});
-gsap.from(".grid-parent-container", {filter: "blur(5px)", rotationX: -16, opacity: 1, duration: 2, delay: 0.1, ease: 'back.inOut'});
-gsap.from(".say-title", {x: 100, opacity: 0, duration: 0.4, delay: 1, ease: 'back.inOut'});
-gsap.from(".goodbye-title", {x: 100, opacity: 0, duration: 0.4, delay: 1.1, ease: 'back.inOut'});
-gsap.from(".to-title", {x: 100, opacity: 0, duration: 0.4, delay: 1.2, ease: 'back.inOut'});
-gsap.from(".boring-title", {x: 100, opacity: 0, duration: 0.4, delay: 1.3, ease: 'back.inOut'});
-gsap.from(".period-title", {scale: 0, opacity: 0, duration: 0.5, delay: 1.4, ease: 'back.inOut'});
-gsap.from(".services", {y: 100, opacity: 0, duration: 0.5, delay: 2.5, ease: 'back.inOut'});
+// let introTl = gsap.timeline({delay: 0.75}); //Un-comment this line of code to turn it back into a timeline
+gsap.from(".grid-parent-container", { filter: "blur(5px)", rotationX: -16, opacity: 1, duration: 2, delay: 0.1, ease: 'back.inOut' });
+gsap.from(".say-title", { x: 100, opacity: 0, duration: 0.4, delay: 1, ease: 'back.inOut' });
+gsap.from(".goodbye-title", { x: 100, opacity: 0, duration: 0.4, delay: 1.1, ease: 'back.inOut' });
+gsap.from(".to-title", { x: 100, opacity: 0, duration: 0.4, delay: 1.2, ease: 'back.inOut' });
+gsap.from(".boring-title", { x: 100, opacity: 0, duration: 0.4, delay: 1.3, ease: 'back.inOut' });
+gsap.from(".period-title", { scale: 0, opacity: 0, duration: 0.5, delay: 1.4, ease: 'back.inOut' });
+gsap.from(".services", { y: 100, opacity: 0, duration: 0.5, delay: 2, ease: 'back.inOut' });
+gsap.from(".emblem-trigger", { scale: 0, opacity: 0, ease: 'back.inOut', delay: 2.5});
 
 
 
 
 
-// FUNCTION TO CREATE GRID ITEMS
-// Create hero backgorund grid containers
+// FUNCTION TO CREATE GRID ITEMS WITHIN PARENT CONTAINER
 
 var container = document.querySelector('.grid-parent-container')
 
@@ -180,22 +137,21 @@ console.log(container.children.length) // 128
 
 
 
-// 3D TRANSFORM ON POINTER MOVE
+// CREATE 3D TRANSFORM ON POINTER MOVE
 
-const main = document.querySelector("body");
+const main = document.querySelector("#grid-bg");
 
 gsap.set("main", { perspective: 650 });
 
-const outerRX = gsap.quickTo(".grid-wrapper", "rotationX", { ease: "power3" });
-const outerRY = gsap.quickTo(".grid-wrapper", "rotationY", { ease: "power3" });
-
+const outerRX = gsap.quickTo(".grid-wrapper", "rotationX", { ease: "power3", });
+const outerRY = gsap.quickTo(".grid-wrapper", "rotationY", { ease: "power3", });
 
 main.addEventListener("pointermove", (e) => {
-  outerRX(gsap.utils.interpolate(15, -15, e.y / window.innerHeight));
-  outerRY(gsap.utils.interpolate(-15, 15, e.x / window.innerWidth));
+    outerRX(gsap.utils.interpolate(15, -15, e.y / window.innerHeight));
+    outerRY(gsap.utils.interpolate(-15, 15, e.x / window.innerWidth));
 });
 
 main.addEventListener("pointerleave", (e) => {
-  outerRX(0);
-  outerRY(0);
+    outerRX(0);
+    outerRY(0);
 });
